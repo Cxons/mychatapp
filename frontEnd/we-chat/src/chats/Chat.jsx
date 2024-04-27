7e5f6a7a69ae0937c7984654e00fdddd1dcb1753
Njoku Chukwuma <njokuchukwuma48@gmail.com>
2024-04-27 21:02:19 +0100

    tried making update quicker

--- frontEnd/we-chat/src/chats/Chat.jsx
+++ frontEnd/we-chat/src/chats/Chat.jsx
@@ -17,15 +17,24 @@ function Chat() {
     "the real messages",
     realMessage
   );
+
   const [loadingState, setLoadingState] = useState(true);
   const [chatText, setChatText] = useState({
     chatText: "",
   });
+  const [totalMessages, setTotalMessages] = useState([]);
   const [chatStatus, setChatStatus] = useState(false);
   const [socketMsg, setSocketMsg] = useState();
   const chatRef = useRef();
   const userName = localStorage.getItem("userName");
   const userId = localStorage.getItem("userId");
+  useEffect(() => {
+    setTotalMessages(realMessage);
+    console.log("the total messages", totalMessages);
+  }, [totalMessages]);
+
+  console.log("the total messages", totalMessages);
+
   console.log("the userName", userName, "the userId", userId);
   function handleChange(e) {
     e.preventDefault();
@@ -53,7 +62,14 @@ function Chat() {
       setSocketMsg(msg);
       console.log("the socket message", socketMsg);
     });
-
+    setTotalMessages([
+      ...totalMessages,
+      {
+        senderId: userId,
+        receiverId: chatContext.chatInfo.recipientId,
+        messageText: chatText.chatText,
+      },
+    ]);
     chatRef.current.value = "";
   }
   console.log("the new socket message", socketMsg);
@@ -63,25 +79,32 @@ function Chat() {
       <div className="flex justify-center w-[100vw] min-h-[100vh]  bg-white flex-col">
         <div className="h-[5rem] w-[100%] bg-gray-100 text-[4rem] "></div>
         <div className=" mt-[4rem]  h-[80vh] w-[100%] overflow-auto no-scrollbar flex flex-col ">
-          {realMessage.map((msg) => {
-            const value = userId == msg.senderId || userId == msg.receiverId;
-            console.log("the hard value", value);
-            return value == false ? (
-              <div key={Math.random()} className="flex max-w-[100vw] ml-[3rem]">
-                <div className="flex justify-start my-2">
-                  <div className="bg-gray-100 text-gray-800 p-2 rounded-lg shadow-md max-w-md">
-                    <p>{msg.messageText}.</p>
+          {totalMessages &&
+            totalMessages.map((msg) => {
+              const value = userId == msg.senderId || userId == msg.receiverId;
+              console.log("the hard value", value);
+              return value == false ? (
+                <div
+                  key={Math.random()}
+                  className="flex max-w-[100vw] ml-[3rem]"
+                >
+                  <div className="flex justify-start my-2">
+                    <div className="bg-gray-100 text-gray-800 p-2 rounded-lg shadow-md max-w-md">
+                      <p>{msg.messageText}.</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <div key={Math.random()} className="flex self-end mr-[4rem] my-2">
-                <div className="bg-blue-500 text-white p-2 rounded-lg shadow-md max-w-md">
-                  <p>{msg.messageText}</p>
+              ) : (
+                <div
+                  key={Math.random()}
+                  className="flex self-end mr-[4rem] my-2"
+                >
+                  <div className="bg-blue-500 text-white p-2 rounded-lg shadow-md max-w-md">
+                    <p>{msg.messageText}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
           {/* {userId == socketMsg.sender ? (
             <div className="flex self-end max-w-[100vw] ml-[3rem]">
               <div className="flex  my-2">
